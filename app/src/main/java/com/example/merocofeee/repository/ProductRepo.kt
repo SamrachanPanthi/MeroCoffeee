f5f1931b3c16b84d454e6c75ebbb9ee0bdc452b7
diya-neupane <439diyaneupane@gmail.com>
2025-12-29 11:50:36 +0545

    added product viewmodel and productrepoimpl

---
+++ app/src/main/java/com/example/merocofeee/repository/ProductRepo.kt
@@ -0,0 +1,30 @@
+import android.content.Context
+import android.net.Uri
+import com.example.merocofeee.model.ProductModel
+
+
+interface ProductRepo {
+
+    fun addProduct(product: ProductModel, callback: (Boolean, String,String?) -> Unit)
+
+    fun updateProduct(productId: String, product: ProductModel, callback: (Boolean, String) -> Unit)
+
+    fun deleteProduct(productId: String, callback: (Boolean, String) -> Unit)
+
+    fun getProductById(productId: String, callback: (Boolean, String, ProductModel?) -> Unit)
+
+    fun getAllProducts(callback: (Boolean, String, List<ProductModel>) -> Unit)
+
+    fun getAllProductsByCategory(category: String, callback: (Boolean, String, List<ProductModel>?) -> Unit)
+
+    fun getAvailableProducts(callback: (Boolean, String, List<ProductModel>) -> Unit)
+
+    fun getAllProductsByUser(userId: String, callback: (Boolean, String, List<ProductModel>) -> Unit)
+
+    fun updateAvailability(productId: String, available: Boolean, callback: (Boolean, String) -> Unit)
+    fun uploadImage (context : Context, imageUri: Uri,callback: ( String?) -> Unit)
+
+    fun getFileNameFromUri(context: Context,uri: Uri):String?
+    fun updateQuantity(productId: String, quantity: Int, callback: (Boolean, String) -> Unit)
+
+}
