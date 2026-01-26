4b798ec45242bbeabbcb1431195f1656d31fc45f
Nitess Katuwal <nitesskatuwal@Nitesss-MacBook-Pro.local>
2026-01-26 21:13:13 +0545

    CreatingNotificationViewModel

--- app/src/main/java/com/example/merocofeee/viewmodel/NotificationViewModel.kt
+++ app/src/main/java/com/example/merocofeee/viewmodel/NotificationViewModel.kt
@@ -10,16 +10,3 @@ import kotlinx.coroutines.launch
 
 class NotificationViewModel : ViewModel() {
 
-    private val repository = NotificationRepository()
-
-    private val _notifications = MutableStateFlow<List<AppNotification>>(emptyList())
-    val notifications: StateFlow<List<AppNotification>> = _notifications
-
-    fun loadNotifications(userId: String) {
-        viewModelScope.launch {
-            repository.getNotifications(userId) { list ->
-                _notifications.value = list.sortedByDescending { it.timestamp }
-            }
-        }
-    }
-}
