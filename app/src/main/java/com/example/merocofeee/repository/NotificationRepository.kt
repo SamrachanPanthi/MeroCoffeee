03bd5a4c4ac0033a969d0e878f22ca38b6a662b1
Nitess Katuwal <nitesskatuwal@Nitesss-MacBook-Pro.local>
2026-01-26 21:07:21 +0545

    NotificationRepo

--- app/src/main/java/com/example/merocofeee/repository/NotificationRepository.kt
+++ app/src/main/java/com/example/merocofeee/repository/NotificationRepository.kt
@@ -9,3 +9,19 @@ import com.google.firebase.database.ValueEventListener
 class NotificationRepository {
     private val database = FirebaseDatabase.getInstance().getReference("notifications")
 
+    fun getNotifications(
+        userId: String,
+        onResult: (List<AppNotification>) -> Unit
+    ) {
+        database.child(userId).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val notifications = snapshot.children.mapNotNull { it.getValue(AppNotification::class.java) }
+                onResult(notifications)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+        })
+    }
+}
