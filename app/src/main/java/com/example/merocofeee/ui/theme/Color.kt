12db9d991faa326ac923c2bb097b813995621bbf
Samrachanapanthi <panthisamrachana@gmail.com>
2026-01-28 17:19:07 +0545

    changes in color.kt

--- app/src/main/java/com/example/merocofeee/ui/theme/Color.kt
+++ app/src/main/java/com/example/merocofeee/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Blue  = Color(0xFF1C2A60)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
- val AccentOrange = Color(0xFFFF9800)
+
 
 
 val ColorOrange = Color(0xFFE19800)
